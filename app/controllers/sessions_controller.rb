3e142c8773b545a27732d2391017a5edcce3ed8d
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 18:38:27 +0530

    Add sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+    render layout: 'slate'
+  end
+
+  def create
+    @user = Authentication.new(user_params)
+
+    if @user.authenticated?
+      sign_in(@user.user)
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Invalid name'
+      render :new
+    end
+  end
 end
