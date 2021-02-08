f329554f78eff192bf74d60dca6d322ae999d807
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 19:26:25 +0530

    Add association between user and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: :creator_id, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
 end
