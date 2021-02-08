53682faf3de4d44edf5de4275730163af2af0622
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 18:31:39 +0530

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
