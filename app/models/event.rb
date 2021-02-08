48ae07afa0bac5a20cbef9acd6308eab91fd981d
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 21:19:20 +0530

    Source event_attendances to attended_event_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
-  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendances, source: :attendee
 
   validates :name, :event_date, presence: true
