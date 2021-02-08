dfb96256c99c844a05d33ed8d73e78c323d9e69d
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 21:09:07 +0530

    Fix sourcing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, dependent: :destroy
 
-  has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
+  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :event_attendances, source: :attended_event
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
