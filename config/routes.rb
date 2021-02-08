afc2ec142c5687224004baf37ef04f7dc837f284
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 19:55:23 +0530

    Initialize events controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
+  resources :events, only: %i[index new create show]
 end
