8f0971ef9cbb0cee181b77f9e80203a62185839d
abeidahmed <abeidahmed92@gmail.com>
2021-02-08 20:57:13 +0530

    Handle many to many association through sourcing

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
-  has_many :event_attendances, dependent: :destroy
-  has_many :attendees, foreign_key: :attendee_id, through: :event_attendances
+  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attendees, through: :event_attendances, source: :attendee
 
   validates :name, :event_date, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, dependent: :destroy
 
-  has_many :event_attendances, dependent: :destroy
-  has_many :attended_event, foreign_key: :attended_event_id, through: :event_attendances
+  has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
+  has_many :attended_events, through: :event_attendances, source: :attended_event
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
 end
